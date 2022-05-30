565df02fc6bca68d677229e0e22743f7ec063e5e
Danasegov <104370574+Danasegov@users.noreply.github.com>
2022-05-30 08:58:19 -0500

    Add files via upload

---
+++ 1er Trimestre/Hola.py.py
@@ -0,0 +1,27 @@
+while True:
+  print("Bienvenido a chatbot Whatsapp")
+  print("ingrese 1 para reporte de fallas, ingrese 2 para pagos, ingrese 3 para promociones ")
+  menu=input("¿Cuál es su problema?:")
+  
+  if (menu=="1"):
+    print("reporte de fallas")
+    print("television o internet")
+    repor=input("¿Cual es su falla?")
+    if (repor=="television"):
+      print("Su número de reporte es 20223467")
+      print("gracias por su reporte,lo tomaremos en cuenta más adelante") 
+    if(repor=="internet"):
+      print("Su número de reporte es 20223467")
+      print("gracias por su reporte,lo tomaremos en cuenta más adelante") 
+      
+  if (menu=="2"):
+    print("Forma de pagos")
+    input("Ingrese número de tarjeta de pago:")
+    print("El pago fue recibido") 
+     
+  if (menu=="3"):
+     print("Promociones")
+     print("recibes el 20% de descuento en pagos en efectivo")
+     print("tienes una promocion en tu tarjeta club de cinepolis, llevate dos combos familia, 2 x 40  ")
+     print("has visitado vidanta unas tres veces al año! tienes una promocion en una habitacion vip a tan solo 10 mil pesos")
+    
