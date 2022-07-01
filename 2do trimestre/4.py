89d4e7f3ca7e5c582579882b7399a3417a927c79
Danasegov <104370574+Danasegov@users.noreply.github.com>
2022-07-01 14:08:42 -0500

    Add files via upload

---
+++ 2do trimestre/4.py
@@ -0,0 +1,12 @@
+materia1=int(input("calificación  "))
+materia2=int(input("calificación  "))
+materia3=int(input("calificación  "))
+resultado=(materia1+materia2+materia3/3)
+
+if  resultado<5:
+  print("reprobados")
+    
+if  resultado>5:
+  input('aprobados') 
+
+
