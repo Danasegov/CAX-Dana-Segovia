fdc96dda09bce6fdad31ac1048af849affcd14c4
Danasegov <104370574+Danasegov@users.noreply.github.com>
2022-07-01 14:12:52 -0500

    Add files via upload

---
+++ 2do trimestre/6.py
@@ -0,0 +1,7 @@
+x=int(input("dame un numero "))
+if x<10:
+  print("smaller")
+    
+if x>20:
+  input('bigger') 
+
---
+++ 2do trimestre/7.py
@@ -0,0 +1,14 @@
+x=5
+if x<10:
+  print('smaller')
+
+age=5
+
+if age((x>=10) and (x<=20)): 
+
+
+if x>20:
+  
+  print('bigger')
+
+  print('finis')
